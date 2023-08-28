c3c2d718cbbfb47e1dd90114eb95e9e76acbdedd
Ankit Srivastava <121329823+sAnkit2004@users.noreply.github.com>
2023-08-28 10:51:41 +0530

    Rewardvalue.java

--- Rewardvalue.java
+++ Rewardvalue.java
@@ -1,46 +1,47 @@
-package java.main;
+package main.java;
 import java.util.Scanner;
- class RewardValue {
-    Scanner sc=new Scanner(System.in);
+public class REwardValue {
+	Scanner sc=new Scanner(System.in);
+	
 
+	REwardValue() // construtor  to accept value in cash
+	{
+	double cash=0;
 
-    RewardValue() // construtor  to accept value in cash
-    {
-        double cash=0;
+	}
+	REwardValue( double miles) //constructor to accept value in miles //
+	{
+		double x=miles;
+	}
+	public  double getMilesValue()  // method to return the cash value //
+	{
+		System.out.println("WELCOME TO CREDIT CARD REWARD CONVERTER");
+		System.out.print("PLEASE ENTER A CASH VALUE TO CONVERT TO AIRLINE MILES- ");
+		double miles=sc.nextDouble();
+	 return miles;
+		
+	}
+	public double getCashValue()  //method to return  how many miles // 
+	{
+				
+		double cash=getMilesValue()*0.0035;
+	 return cash;
+	}
+	
+	public static void main(String[] args) {  // converting the miles into the cash //
+		//TODO Auto-generated method stub
+		REwardValue obj=new REwardValue();
+		double input_value=obj.getMilesValue();
+		 double p=obj.getCashValue();
+		
+		System.out.println("$"+ p + " is worth "+ input_value + "  miles");;
+		
+		
+		
+				
+				
+		
 
-    }
-    RewardValue( double miles) //constructor to accept value in miles //
-    {
-        double x=miles;
-    }
-    public  double getCashValue()  // method to return the cash value //
-    {
-        System.out.println("enter  value in cash");
-        double cash=sc.nextDouble();
-        return cash;
-
-    }
-    public double getMilesValue()  //method to return  how many miles //
-    {
-        System.out.println("enter  how many miles the REwardValue is worth");
-        double miles=sc.nextDouble();
-        return miles;
-    }
-
-    public static void main(String[] args) {  // converting the miles into the cash //
-        // TODO Auto-generated method stub
-        RewardValue obj=new RewardValue();
-        obj.getCashValue();
-        double p=obj.getMilesValue();
-
-        System.out.println("converting miles value to cash value"+p*0.0035);;
-
-
-
-
-
-
-
-    }
+	}
 
 }
